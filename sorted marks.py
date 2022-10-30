90999f232436bd6a575e271fa76b14e6a0561716
Argha818 <112699194+Argha818@users.noreply.github.com>
2022-10-30 13:18:54 +0530

    Create sorted marks.py

---
+++ sorted marks.py
@@ -0,0 +1,9 @@
+m1=int(input("Enter the marks of first student: "))
+m2=int(input("Enter the marks of  second student: "))
+m3=int(input("Enter the marks of  third student : "))
+m4=int(input("Enter the marks of  fourth student: "))
+m5=int(input("Enter the marks of fifth student: "))
+m6=int(input("Enter the marks of  sixth student: "))
+mylist=[m1,m2,m3,m4,m5,m6]
+mylist.sort()
+print(mylist)
